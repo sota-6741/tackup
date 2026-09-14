d81236227ebc3916da8f2800a6d06dbe98b41757
sota-6741 <207503862+sota-6741@users.noreply.github.com>
2026-09-15 06:55:03 +0900

    feat: ForbiddenError を追加

--- src/shared/domain/errors.ts
+++ src/shared/domain/errors.ts
@@ -8,3 +8,5 @@ export class DomainError extends Error {
 export class ValidationError extends DomainError {}
 
 export class NotFoundError extends DomainError {}
+
+export class ForbiddenError extends DomainError {}
